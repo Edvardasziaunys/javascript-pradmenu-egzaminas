d5e7b6d51a13ad45838ff73e67cdccbc55a73db2
Edvardas.z <edvardas.ziaunys@gmail.com>
2024-03-30 13:35:34 +0200

    navigation active scroll event js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("JS egazaminas start, test change");
+// navigation scroll active
+
+const sections = document.querySelectorAll("section");
+const navA = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  let currentSectionId = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (
+      scrollY >= sectionTop - 100 &&
+      scrollY < sectionTop + sectionHeight - 100
+    ) {
+      currentSectionId = section.getAttribute("id");
+    }
+  });
+  navA.forEach((a) => {
+    if (a.getAttribute("href") === `#${currentSectionId}`) {
+      a.classList.add("active");
+    } else {
+      a.classList.remove("active");
+    }
+  });
+});
+
+// tab section changing
