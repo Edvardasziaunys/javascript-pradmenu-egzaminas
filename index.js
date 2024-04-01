8d026baa2362b98ead71b21214712a159d27f937
Edvardas.z <edvardas.ziaunys@gmail.com>
2024-04-01 20:08:03 +0300

    adjusted tab section style, still need to do the photos when tabs are changing

--- index.js
+++ index.js
@@ -26,3 +26,21 @@ window.addEventListener("scroll", () => {
 });
 
 // tab section changing
+
+function toggleTab(tabNumber) {
+  document.querySelectorAll(".changing-tab").forEach((tab) => {
+    tab.style.display = "none";
+  });
+  let selectedTab = document.getElementById(`changing-tabs-${tabNumber}`);
+  if (selectedTab) {
+    selectedTab.style.display = "flex";
+  }
+  document.querySelectorAll(".tab-button").forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  let clickedButton = document.getElementById(`tabButton${tabNumber}`);
+  if (clickedButton) {
+    clickedButton.classList.add("active");
+  }
+}
